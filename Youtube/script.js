2c77afb161cd62ea9b16f2edab70133fb63ae7f9
HyeLee <39701029+hlpark0209@users.noreply.github.com>
2022-01-14 22:14:22 +0900

    Update js scroll function

--- Youtube/script.js
+++ Youtube/script.js
@@ -1,1 +1,26 @@
 'use strict';
+
+const toggleBtn = document.querySelector('.fa-caret-down');
+const title = document.querySelector('.info__title h4');
+
+
+// title toggle
+// toggleBtn.addEventListener('click', () => {
+
+
+// });
+
+//make header fixed when scroll
+const header = document.querySelector('.header');
+const video = document.querySelector('.view');
+const headerHeight = header.getBoundingClientRect().height;
+console.log(headerHeight);
+
+document.addEventListener('scroll', () => {
+    
+    if(window.scrollY > headerHeight) {
+        video.classList.add('header-white');
+    } else{
+        video.classList.remove('header-white');
+    }
+});
