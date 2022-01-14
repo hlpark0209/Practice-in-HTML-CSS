c9faafd33dd1aa2b90e53b3b5266f691b6156693
HyeLee <39701029+hlpark0209@users.noreply.github.com>
2022-01-14 16:29:23 +0900

    final touch

--- navBar/script.js
+++ navBar/script.js
@@ -1,10 +1,8 @@
 'use strict';
 
-const toggleBtn = document.querySelector('.fa-bars');
+const toggleBtn = document.querySelector('.toggleBtn');
 const menu = document.querySelector('.menu');
 const contact = document.querySelector('.contact');
-
-
 
 function mobileMenu(btn){
     menu.classList.toggle('active');
