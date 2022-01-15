d43e0f1a4d4f4dcde203f94f2d359c8f2b5778c5
HyeLee <39701029+hlpark0209@users.noreply.github.com>
2022-01-15 15:21:29 +0900

    final solution
    - video scroll
    - update toggle function

--- Youtube/script.js
+++ Youtube/script.js
@@ -5,22 +5,10 @@ const title = document.querySelector('.info__title h4');
 
 
 // title toggle
-// toggleBtn.addEventListener('click', () => {
+toggleBtn.addEventListener('click', () => {
+    title.classList.toggle('clamp');
+    toggleBtn.classList.toggle('rotate');
 
-
-// });
-
-//make header fixed when scroll
-const header = document.querySelector('.header');
-const video = document.querySelector('.view');
-const headerHeight = header.getBoundingClientRect().height;
-console.log(headerHeight);
-
-document.addEventListener('scroll', () => {
-    
-    if(window.scrollY > headerHeight) {
-        video.classList.add('header-white');
-    } else{
-        video.classList.remove('header-white');
-    }
 });
+
+
